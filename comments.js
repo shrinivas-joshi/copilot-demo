4dc27eb149f4568d9cdd1ebe3a3b374d0ab1e61f
Shrinivas Joshi <shrinivas23.git@gmail.com>
2024-02-28 23:30:35 +0530

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,57 +1,40 @@
 //create web server
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var querystring = require('querystring');
-var comments = [];
+const express = require('express');
+const app = express();
+const port = 3000;
 
-http.createServer(function(req, res){
-    //parse url
-    var urlObj = url.parse(req.url, true);
-    var pathname = urlObj.pathname;
-    //parse query
-    var query = urlObj.query;
-    //handle static resource request
-    if(pathname === '/'){
-        pathname = '/index.html';
-    }
-    var extname = path.extname(pathname);
-    if(pathname !== '/favicon.ico'){
-        fs.readFile('static/' + pathname, function(err, data){
-            if(err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end('404 Not Found');
-            }
-            var mime = getMime(extname);
-            res.writeHead(200, {'Content-Type': mime});
-            res.end(data);
-        });
-    }
-    //handle ajax request
-    if(pathname === '/comment'){
-        var str = '';
-        req.on('data', function(chunk){
-            str += chunk;
-        });
-        req.on('end', function(){
-            var obj = querystring.parse(str);
-            comments.push(obj);
-            res.end(JSON.stringify(comments));
-        });
-    }
-}).listen(8080);
+//import comments
+const comments = require('./comments');
 
-function getMime(extname){
-    switch(extname){
-        case '.html':
-            return 'text/html';
-        case '.css':
-            return 'text/css';
-        case '.js':
-            return 'text/javascript';
-        default:
-            return 'text/html';
+//create a route
+app.get('/comments', (req, res) => {
+    res.json(comments);
+});
+
+//start server
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
+// Path: comments.js
+module.exports = [
+    {
+        id: 1,
+        body: "Love this!"
+    },
+    {
+        id: 2,
+        body: "Super good"
+    },
+    {
+        id: 3,
+        body: "You are the best"
+    },
+    {
+        id: 4,
+        body: "Ramen is my fav food ever"
+    },
+    {
+        id: 5,
+        body: "Nice nice nice!"
     }
-}
-console.log('Server is running at http://')
+];
